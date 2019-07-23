34a9f04ea82332cf4af51fa44d109e8b3c3177e9
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-07-22 20:28:52 -0400

    edited buttons

--- client/src/components/Form/FormButton.js
+++ client/src/components/Form/FormButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./FormButton.css";
 
 export const FormButton = props => (
-  <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn">
+  <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btnColor">
     {props.children}
   </button>
 );
